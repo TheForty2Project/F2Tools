a5c63c24d2dac39afc1c5d66f1ee2a03986a7a08
Bobi <bobi@the.forty2project.org>
2026-04-26 00:43:51 +0200

    - moving methods - eslint config

--- src/f2yamlLinkExtractor.ts
+++ src/f2yamlLinkExtractor.ts
@@ -1,3 +1,4 @@
+import * as yaml from 'yaml';
 import * as vscode from 'vscode';
 import { Data } from './Data';
 import { VsCodeUtils, Message } from './VsCodeUtils';
@@ -8,11 +9,11 @@ import { StringOperation } from './StringOperations';
 export class F2yamlLinkExtractor {
   protected static extractedSymbols: Array<string> = [];
 
-  public static async createF2YamlSummaryLink(activeDoc: vscode.TextDocument, cursorPosition: vscode.Position) {    
+  public static async createF2YamlSummaryLink(activeDoc: vscode.TextDocument, cursorPosition: vscode.Position) {
     return this.createF2Link(activeDoc, cursorPosition, false);
   }
 
-  static async createF2YamlIdLink(activeDoc: vscode.TextDocument, cursorPosition: vscode.Position) {    
+  static async createF2YamlIdLink(activeDoc: vscode.TextDocument, cursorPosition: vscode.Position) {
     return this.createF2Link(activeDoc, cursorPosition, true);
   }
 
@@ -22,17 +23,100 @@ export class F2yamlLinkExtractor {
     filePath = StringOperation.removeExtension(filePath);
     let yamlPath = await this.getYamlPath(activeDoc, cursorPosition, idLink);
     return Data.PATTERNS.START_OF_F2YAML_LINK + filePath + "\\" + "." + yamlPath + Data.PATTERNS.END_OF_F2YAML_LINK;
-  }  
+  }
 
   static async getYamlPath(activeDoc: vscode.TextDocument, cursorPosition: vscode.Position, idLink: boolean) {
     let yamlPath = '';
     let yamlKeys = await this.getYamlKeys(activeDoc, cursorPosition);
-    let yamlKeyValues = await YamlTaskOperations.getYamlKeyValues(yamlKeys, idLink, activeDoc);
+    let yamlKeyValues = await this.getPropertyValuesFor(yamlKeys, idLink, activeDoc);
     let betterDots = this.moveDots(yamlKeyValues);
     let yamlParts: string[] = this.removeStatus(betterDots);
     return yamlPath = yamlParts.join('.');
   }
-  static moveDots(yamlKeyValues: string[]) { // TODO move this from here
+
+  static async getPropertyValuesFor(yamlKeys: string[], idLink: boolean, activeDoc: vscode.TextDocument): Promise<string[]> {
+    let yamlKeyValues: string[] = [];
+    const yamlDoc = await YamlTaskOperations.parseYamlDoc(activeDoc.uri);
+    let parentYamlObj: any = yamlDoc.get(yamlKeys[0], true);
+    let parentKeyValue = this.getParentValue(parentYamlObj, idLink ? "Id" : "", yamlKeys[0]);
+    yamlKeyValues.push(parentKeyValue);
+
+    for (let index = 1; index < yamlKeys.length; index++) {
+      const yamlKey = yamlKeys[index];
+      const yamlObj = await this.getYamlObjFromParentObj(yamlKey, yamlDoc, parentYamlObj);
+      const yamlKeyValue = await this.getYamlKeyValueBasedOnKeyType(yamlObj, idLink ? "Id" : "");
+      if (!yamlKeyValue) {
+        const yamlKeySummary = StringOperation.wrapInQuotesIfMultiWord(yamlKey);
+        yamlKeyValues.push(yamlKeySummary);
+        parentYamlObj = yamlObj;
+        continue;
+      }
+      yamlKeyValues.push(yamlKeyValue);
+      parentYamlObj = yamlObj;
+    }
+    // first I need to get the actual yaml-obj from the yamlKeys
+    // second I need to get the value of the yamlkeys form the objects and there if the yamlKeyType is not found then give the summary instead of the value of the key
+    return yamlKeyValues;
+  }
+
+  private static getParentValue(parentYamlObj: any, yamlKeyType: string, yamlKey: string) {
+    let parentKeyValue = this.getYamlKeyValueBasedOnKeyType(parentYamlObj, yamlKeyType);
+    let parentKeySummary;
+    if (!parentKeyValue) {
+      if (yamlKey.startsWith('.')) {
+        parentKeyValue = this.TheDotSettelment(yamlKey);
+        return parentKeyValue
+      }
+      parentKeySummary = StringOperation.wrapInQuotesIfMultiWord(yamlKey);
+      parentKeyValue = parentKeySummary;
+    }
+    return parentKeyValue;
+  }
+
+  private static TheDotSettelment(yamlKey: string) { // TODO move to stringOperations
+    let withoutdot = yamlKey.slice(1);
+    let newWord = StringOperation.wrapInQuotesIfMultiWord(withoutdot);
+    newWord = '.' + newWord;
+    return newWord;
+  }
+
+
+  static getYamlObjFromParentObj(yamlKey: string, yamlDoc: yaml.Document<yaml.Node, true>, parentYamlObj: any) {
+    let yamlObj: any;
+    let yamlObjItems = parentYamlObj.items;
+    if (!yamlObjItems) yamlObjItems = parentYamlObj.value.items;
+    for (const item of yamlObjItems) {
+      if (yamlKey == item.key.value) yamlObj = item;
+    }
+    return yamlObj;
+  }
+
+  static getYamlKeyValueBasedOnKeyType(yamlObj: any, yamlKeyType: string) { // TODO 
+    let yamlKeyValue;
+    if (!yamlKeyType || !yamlObj.value) return;
+    try {
+      for (const item of yamlObj.value.items) {
+        if (item.key.value == yamlKeyType) {
+          yamlKeyValue = item.value.value;
+        }
+      }
+    } catch (error1: any) {
+      try {
+
+        for (const item of yamlObj.items) {
+          if (item.key.value == yamlKeyType) {
+            yamlKeyValue = item.value.value;
+          }
+        }
+      } catch (error2: any) {
+        //Message.err(error1.message + error2.message);
+      }
+    }
+    if (yamlKeyType == "Id" && yamlKeyValue != undefined) return "." + yamlKeyValue; // a temp mesue
+    return yamlKeyValue;
+  }
+
+  static moveDots(yamlKeyValues: string[]) {
     let betterDots = [];
     for (const keyValue of yamlKeyValues) {
       if (keyValue.startsWith('".')) {
